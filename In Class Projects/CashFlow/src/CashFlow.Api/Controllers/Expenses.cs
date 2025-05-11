9f7a91e4f650ccc339194b8c1a5cf1c4e2f26eac
Ricardo <jricardo.sa16@gmail.com>
2025-05-10 22:42:27 -0300

    new project

---
+++ In Class Projects/CashFlow/src/CashFlow.Api/Controllers/Expenses.cs
@@ -0,0 +1,20 @@
+﻿using CashFlow.Application.UseCases.Expenses.Register;
+using CashFlow.Communication.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CashFlow.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class Expenses : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register([FromBody] RequestRegisterExpenseJson request)
+    {
+        var UseCase = new RegisterExpenseUseCase();
+
+        var response = UseCase.Execute(request);
+
+        return Created();
+    }
+}
---
+++ In Class Projects/CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,28 @@
+﻿using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisteredExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(f => f.ErrorMessage).ToList();
+            
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
