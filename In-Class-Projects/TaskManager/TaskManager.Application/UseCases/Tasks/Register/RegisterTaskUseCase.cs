f1ef403b9e5296643cdcb7ec88bac55b57876769
Ricardo <jricardo.sa16@gmail.com>
2025-05-13 22:46:59 -0300

    Task Manager Project

---
+++ In-Class-Projects/TaskManager/TaskManager.Application/UseCases/Tasks/Register/RegisterTaskUseCase.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager.Communication.Requests;
+using TaskManager.Communication.Responses;
+
+namespace TaskManager.Application.UseCases.Tasks.Register;
+public class RegisterTaskUseCase
+{
+    public ResponseRegisterTaskJson Execute(RequestRegisterTaskJson request)
+    {
+        return new ResponseRegisterTaskJson();
+    }
+}
