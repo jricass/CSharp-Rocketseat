7ccd4c779aa2973b2f8b0c31e75ada3fbd159b4c
Ricardo <jricardo.sa16@gmail.com>
2025-05-14 22:52:11 -0300

    starting with databases :)

---
+++ In-Class-Projects/CashFlow/src/CashFlow.Infrastructure/DataAccess/CashFlowDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CashFlow.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CashFlow.Infrastructure.DataAccess;
+public class CashFlowDbContext : DbContext
+{
+    public DbSet<Expense> Expenses { get; set; } // Mesmo nome da tabela; Da acesso a toda a tabela respectiva
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = "Server=localhost;Database=cashflowdb;Uid=root;Pwd=@Password123";
+
+        var version = new Version(8, 0, 42);
+
+        var serverVersion = new MySqlServerVersion(version);
+
+        optionsBuilder.UseMySql(connectionString, serverVersion);
+    }
+}
