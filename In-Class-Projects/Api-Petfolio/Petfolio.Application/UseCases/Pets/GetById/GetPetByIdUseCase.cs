f5df732fd8053dae0d96d2d8d87ec8896fe2f532
Ricardo <jricardo.sa16@gmail.com>
2025-06-24 21:16:00 -0300

    In class projects

---
+++ In-Class-Projects/Api-Petfolio/Petfolio.Application/UseCases/Pets/GetById/GetPetByIdUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pets.GetById;
+
+public class GetPetByIdUseCase
+{
+    public ResponsePetJson Execute(int id)
+    {
+        return new ResponsePetJson
+        {
+            id = id,
+            Name = "Pipoca",
+            Type = Communication.Enums.PetType.Cat,
+            Birthday = new DateTime(year: 2023, month: 01, day: 01)
+        }; 
+    }
+}
---
+++ In-Class-Projects/Api-Petfolio/Petfolio.Application/UseCases/Pets/Register/RegisterPetUseCase.cs
@@ -0,0 +1,20 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.Application.UseCases.Pets.Register;
+
+// Primeira Regra de Negócio
+// Funções para Registrar um 'Pet'
+// Só existirá UMA função pública! As demais serão privadas e auxiliarão a regra de negócio
+// As funções públicas serão chamadas de 'Execute'
+public class RegisterPetUseCase
+{
+    public ResponseRegisterPetJson Execute(RequestPetJson request)
+    {
+        return new ResponseRegisterPetJson
+        {
+            Id = 7,
+            Name = request.Name,
+        };
+    }
+}
